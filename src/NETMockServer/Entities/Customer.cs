aaa664197eea9e9206d2913699b3a860ffdaf343
Angelo Pirola <angelo9871@gmail.com>
2026-01-20 00:44:26 +0100

    Add base entity, Customer, and Product classes
    Introduced EntityBase as an abstract class with a Guid Id property. Added Customer and Product entities inheriting from EntityBase, including relevant properties for each. These classes establish the foundational data model for customers and products in the application.

---
+++ src/NETMockServer/Entities/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace NETMockServer.Entities;
+
+public class Customer : EntityBase
+{
+    public string FirstName { get; set; } = default!;
+    public string LastName { get; set; } = default!;
+    public string Email { get; set; } = default!;
+    public DateTime RegisteredAt { get; set; }
+}
---
+++ src/NETMockServer/Entities/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace NETMockServer.Entities;
+
+public class Product : EntityBase
+{
+    public string Name { get; set; } = default!;
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
